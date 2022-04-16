cf5f3b0b80eb5e06a4a63fb888a3e32ed15a0b8a
banadoras <64110552+banadoras@users.noreply.github.com>
2022-04-16 12:43:55 -0400

    Initial commit

---
+++ src/movies.js
@@ -0,0 +1,25 @@
+const movies = [
+  "The terminal",
+  "Inception",
+  "Dark Knight rises",
+  "The Hunchback of Notredame city",
+  "Moana",
+  "Sing",
+  "Sing Two",
+  "The Incredibles",
+  "Luca",
+  "Pocahantas",
+  "Cindrella",
+  "Beauty and the beast",
+  "Frozen",
+  "Frozen Two",
+  "Snow white and the seven dwarfs",
+  "Encanto",
+  "Grinch",
+  "Monsters",
+  "Wild hearts cannot be broken",
+  "Home alone",
+  "Home alone two"
+];
+
+export default movies;
